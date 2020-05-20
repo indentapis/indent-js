d9c97c869126c32962c6795e6473bd9054bc368d
Fouad Matin <m@fouad.org>
2020-05-20 12:34:25 -0700

    add(packages/webhook): error on failed verify

--- packages/webhook/test/signature.test.ts
+++ packages/webhook/test/signature.test.ts
@@ -1,4 +1,4 @@
-import { sign, getSignaturePayload } from '../src'
+import { sign, verify, getSignaturePayload } from '../src'
 
 describe('signature', () => {
   test('smoke test - static equals', async () => {
@@ -21,5 +21,54 @@ describe('signature', () => {
     return expect(sig).toBe(
       'c27f187960625ebccc33336030b071dac1d54b1e98d9e27fe5452ee00c935fb0'
     )
+  })
+
+  test('smoke test - verify with static', async () => {
+    let events = [
+      {
+        event: 'access/grant',
+        timestamp: '2020-05-01T07:00:00Z'
+      }
+    ]
+
+    let error
+
+    try {
+      await verify({
+        secret: 'secret_123',
+        signature:
+          'c27f187960625ebccc33336030b071dac1d54b1e98d9e27fe5452ee00c935fb0',
+        timestamp: events[0].timestamp,
+        body: JSON.stringify({ events })
+      })
+    } catch (err) {
+      error = err
+    }
+
+    expect(error).toBeFalsy()
+  })
+
+  test('smoke test - verify should error', async () => {
+    let events = [
+      {
+        event: 'access/grant',
+        timestamp: '2020-05-01T07:00:00Z'
+      }
+    ]
+
+    let error
+
+    try {
+      await verify({
+        secret: 'secret_123',
+        signature: '123',
+        timestamp: events[0].timestamp,
+        body: JSON.stringify({ events })
+      })
+    } catch (err) {
+      error = err
+    }
+
+    expect(error).toBeTruthy()
   })
 })
